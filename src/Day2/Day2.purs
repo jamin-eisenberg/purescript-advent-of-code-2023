5056ebf3d6772ecd5ec3358c0be18dca0493fb54
Jamin Eisenberg <109154023+jeisenberg-chwy@users.noreply.github.com>
2024-11-21 17:02:32 -0500

    d2p2

--- src/Day2/Day2.purs
+++ src/Day2/Day2.purs
@@ -2,16 +2,15 @@ module Day2 where
 
 import Prelude
 
-import Data.Array (all, filter, fromFoldable)
+import Data.Array (all, concat, concatMap, filter, fromFoldable)
 import Data.Either (Either(..))
-import Data.Foldable (sum)
+import Data.Foldable (maximum, product, sum)
 import Data.Generic.Rep (class Generic)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Show.Generic (genericShow)
 import Debug (spyWith)
 import Effect (Effect)
 import Main (run)
-import Options.Applicative.Internal.Utils (lines)
 import Parsing (Parser)
 import Parsing.Combinators (choice, sepBy)
 import Parsing.String (string)
@@ -21,16 +20,23 @@ import Partial (crashWith)
 main ∷ Effect Unit
 main = run 2 Nothing parser calc
 
+calc ∷ Array { id ∷ Int, rounds ∷ Array (Array { color :: Color, count :: Int }) } → Either String Int
 calc lines =
   let
     _ = spyWith "" show lines
   in
-    filter possible lines
-      # map _.id
+    -- filter possible lines
+    --   # map _.id
+    --   # sum
+    --   # Right
+    lines
+      # map power
       # sum
       # Right
 
 data Color = Red | Green | Blue
+
+derive instance Eq Color
 
 derive instance Generic Color _
 instance Show Color where
@@ -39,6 +45,17 @@ instance Show Color where
 type Draw = { color :: Color, count :: Int }
 type Round = Array Draw
 type Game = { id :: Int, rounds :: Array Round }
+
+power :: Game -> Int
+power { rounds } =
+  let
+    draws = concat rounds
+    maxOf c = filter (_.color >>> eq c) draws
+      # map _.count
+      # maximum
+      # fromMaybe 0
+  in
+    maxOf Red * maxOf Green * maxOf Blue
 
 possible :: Game -> Boolean
 possible { rounds } =
